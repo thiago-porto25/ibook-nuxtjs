1d6ee67c518aeb1b5c70d668faf7a0a118072f2f
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-10 10:41:04 -0300

    install axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
