1f491346abe9c92420c7f1a5229da7f4eeeb63e6
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-10 15:29:08 -0300

    finish book page

---
+++ pages/book/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <BookDetailsTemplate />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+export default Vue.extend({
+  layout: 'ibook',
+
+  async asyncData({ params }) {
+    await books.show({ id: params.id as any })
+  },
+
+  head() {
+    return {
+      title: books.$single.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: books.$single.description
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
