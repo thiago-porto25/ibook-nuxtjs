278ae873ab457883a44743bed8b92b41a47c18fa
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-11 11:01:29 -0300

    start work on read page

---
+++ pages/book/_id/read.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ $book }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { books } from '@/store'
+
+export default Vue.extend({
+  computed: {
+    $book() {
+      return books.$single
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
