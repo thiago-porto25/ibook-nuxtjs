b061c51a4ec31c953ede0a518ef32366eb146d00
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-11 14:41:06 -0300

    finish rm unecessary auth logic

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -1,15 +1,12 @@
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import Books from '@/store/books'
-import Auth from '@/store/auth'
 
 // eslint-disable-next-line import/no-mutable-exports
 let books: Books
-let auth: Auth
 
 function initializeStores(store: Store<any>): void {
   books = getModule(Books, store)
-  auth = getModule(Auth, store)
 }
 
-export { initializeStores, books, auth }
+export { initializeStores, books }
