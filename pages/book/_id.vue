4a0a00c7db97859acb7ea6bccb3d5d97b6cb50a2
thiago-porto25 <thiago_porto25@hotmail.com>
2021-09-10 21:06:02 -0300

    finish validation for dynamic routes

--- pages/book/_id.vue
+++ pages/book/_id.vue
@@ -5,12 +5,17 @@
 <script lang="ts">
 import Vue from 'vue'
 import { books } from '@/store'
+import { Book } from '@/models/'
 
 export default Vue.extend({
   layout: 'ibook',
 
   async asyncData({ params }) {
     await books.show({ id: params.id as any })
+  },
+
+  validate({ params }) {
+    return books.$all.some((book: Book) => book.id === parseInt(params.id, 10))
   },
 
   head() {
